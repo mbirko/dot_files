4b28d3ce7dd2bbc602d81a531708ac1bba0518e8
birk <github@mbirk.dk>
2025-04-03 16:34:41 +0200

    feat: added python lsp and automatic venv discovery

--- neovim/lua/custom/lazy.lua
+++ neovim/lua/custom/lazy.lua
@@ -31,7 +31,8 @@ lazy.setup({
   require('custom.plugins.fsharp'),
   require('custom.plugins.cmp'),
   require('custom.plugins.snippets'),
-  require('custom.plugins.indent-blankline')
+  require('custom.plugins.indent-blankline'),
+  --  require('custom.plugins.vimwiki'),
 }, {
   defaults = { lazy = true, },
   performance = {
@@ -39,4 +40,69 @@ lazy.setup({
       reset = false,
     },
   },
+})
+---sets the PYTHONPATH env variable bysearching upward for `.venv` folder and fiding the site-packages within that
+---if no `.venv` is found, its sets to user-site
+local function set_python_venv()
+  ---wraps find returning signle string path or nil
+  ---@param path_name string
+  ---@param search_path string
+  ---@param stop_path string
+  ---@param upward boolean
+  ---@return any
+  local function find_path_to_dir(path_name, search_path, stop_path, upward)
+    local venv_path = vim.fs.find(path_name, {
+      upward = upward,
+      type = "directory",
+      path = search_path,
+      stop = stop_path
+    })
+
+    if venv_path and #venv_path > 0 then
+      vim.notify("found path: " .. venv_path[1], vim.log.levels.TRACE)
+      return venv_path[1]
+    else
+      vim.notify("WARN: " .. path_name .. "was not found", vim.log.levels.WARN)
+      return nil
+    end
+  end
+
+  -- Check if PYTHONPATH is already set, to not search more then necessary
+  if vim.env.PYTHONPATH ~= nil and vim.env.PYTHONPATH ~= "" then
+    vim.notify("PYTHONPATH was not nil or empty: " .. vim.env.PYTHONPATH, vim.log.levels.TRACE)
+    return
+  end
+
+  -- get current working directory
+  local search_path = vim.fn.getcwd()
+  -- stopping searching upward at home directory
+  local stop_path = vim.env.HOME
+  -- find path of virtual enviorment searching upward
+  local venv_path = find_path_to_dir(".venv", search_path, stop_path, true)
+  if venv_path then
+    -- finding site-packages path within environment
+    local python_path = find_path_to_dir("site-packages", venv_path, stop_path, false)
+    if python_path then
+      vim.env.PYTHONPATH = python_path
+      vim.notify("PYTHONPATH is set to: " .. python_path, vim.log.levels.OFF)
+    else
+      -- this sould not happen, just to be sure
+      python_path = vim.fn.system("python3 -m site --user-site")
+      vim.env.PYTHONPATH = python_path
+      vim.notify("site-packages was not found inside .venv, using global instead: " .. python_path, vim.log.levels.WARN)
+    end
+  else
+    -- No virtual enviorment found
+    -- setting path to global installation
+    local python_path = vim.fn.system("python3 -m site --user-site")
+    vim.env.PYTHONPATH = python_path
+    vim.notify("sno venv was found, using global instead: " .. python_path, vim.log.levels.WARN)
+  end
+end
+-- creating filetype autocommand to set python path when a python file is opened
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = "python",
+  callback = function(event)
+    set_python_venv()
+  end
 })
--- neovim/lua/custom/plugins/lsp.lua
+++ neovim/lua/custom/plugins/lsp.lua
@@ -41,7 +41,7 @@ return {
     config = function()
       configure_lsp_server('fsautocomplete')         -- NOTE: Fsharp auto complete
       configure_lsp_server('fsharp_language_server') -- NOTE: Fsharp language server
-
+      -- configure_lsp_server('dprint',
       configure_lsp_server('lua_ls')                 -- NOTE: Lua language server
       configure_lsp_server('ts_ls')                  -- NOTE: TypeScript language server
       configure_lsp_server('eslint')                 -- NOTE: JavaScript language server
@@ -49,8 +49,43 @@ return {
         -- NOTE: all supported filetypes (see :help lspconfig-all) without protobuf
         filetypes = { 'c', 'cpp', 'objc', 'objcpp', 'cuda' },
       })
-      configure_lsp_server('hls') -- NOTE: haskell-language-server
-      configure_lsp_server('pyright') -- NOTE: python-language-server
+
+      -- configure_lsp_server('hls') -- NOTE: haskell-language-server
+
+      configure_lsp_server('pylsp', {
+        settings = {
+          pylsp = {
+            cmd = { "pylsp" },
+            plugins = {
+              pycodestyle = {
+                enabled = false,
+                ignore = { 'W391' },
+                maxLineLength = 100
+              },
+
+              ruff = {
+                enabled = true,       -- Enable the plugin
+                formatEnabled = true, -- Enable formatting using ruffs formatter
+                -- extendSelect = { "I" },  -- Rules that are additionally used by ruff
+                -- extendIgnore = { "C90" },  -- Rules that are additionally ignored by ruff
+                -- format = { "I" },  -- Rules that are marked as fixable by ruff that should be fixed when running textDocument/formatting
+                -- severities = { ["D212"] = "I" },  -- Optional table of rules where a custom severity is desired
+                unsafeFixes = false, -- Whether or not to offer unsafe fixes as code actions. Ignored with the "Fix All" action
+
+                -- Rules that are ignored when a pyproject.toml or ruff.toml is present:
+                --
+                lineLength = 88,              -- Line length to pass to ruff checking and formatting
+                exclude = { "__about__.py" }, -- Files to be excluded by ruff checking
+                select = {},                  -- Rules to be enabled by ruff
+                ignore = {},                  -- Rules to be ignored by ruff
+                -- perFileIgnores = { ["__init__.py"] = "CPY001" },  -- Rules that should be ignored for specific files
+                preview = false,              -- Whether to enable the preview style linting and formatting.
+                targetVersion = "py310",      -- The minimum python version to target (applies for both linting and formatting).
+              },
+            }
+          }
+        }
+      }) -- NOTE: python-language-server
       -- configure_lsp_server('rust_analyzer') -- NOTE: rust-analyzer
       -- configure_lsp_server('texlab')
       configure_lsp_server('nixd', {
