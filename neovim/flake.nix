888f0a1985983b08baece5d67c7bade872de7496
birk <github@mbirk.dk>
2026-05-27 11:25:27 +0200

    Added a flake with a dev enviroment

---
+++ neovim/flake.nix
@@ -0,0 +1,23 @@
+{
+  description = "A very basic flake";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+  };
+
+  outputs =
+    { self, nixpkgs }:
+    let
+      system = "x86_64-linux";
+      pkgs = nixpkgs.legacyPackages.${system};
+
+    in
+    {
+      devShells.${system}.deafault = pkgs.mkShell {
+        nativeBuildInputs = [
+          pkgs.lua-language-server
+        ];
+      };
+
+    };
+}
